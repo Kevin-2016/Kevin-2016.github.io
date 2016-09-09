c5d82020b5fb1a237db0ce019e0f49ccc2962eb9
kwin <wangkai_2015@126.com>
2016-09-09 17:55:56 +0800

    Site updated: 2016-09-09 17:55:54

--- README.md
+++
@@ -1,2 +0,0 @@
-# Kevin-2016.github.io
-kwi的个人博客！
---
+++ js/BaiduStatistics.js
@@ -0,0 +1,9 @@
+
+	var _hmt = _hmt || [];
+	(function() {
+	  var hm = document.createElement("script");
+	  hm.src = "//hm.baidu.com/hm.js?280dc3c29b618758d836e5d0ed9eadae";
+	  var s = document.getElementsByTagName("script")[0]; 
+	  s.parentNode.insertBefore(hm, s);
+	})();
+
